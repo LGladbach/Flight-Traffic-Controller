26380cc18633aafa326cc1a7f9a563991edfcd97
Laurenz Gladbach <laurenz2@mail.uni-paderborn.de>
2024-08-30 18:09:40 +0200

    process: created file for Testing environment

---
+++ Test_trash/Test_Airspace_Runway_Plane.py
@@ -0,0 +1,12 @@
+from src.Airspace import Airspace
+from src.Runway import Runway
+from src.Plane import Plane
+
+#Test Runways
+rws = [Runway((10, 10), (15, 15))]
+
+#Test Planes
+pls = [Plane((0, 0), 0.0)]
+
+#Test Airspace
+ap = Airspace((20, 20))
