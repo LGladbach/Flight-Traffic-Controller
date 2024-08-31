9ee10088f5662da925d399d0861b3cd3ea3e9c4d
Laurenz Gladbach <laurenz2@mail.uni-paderborn.de>
2024-08-31 13:19:10 +0200

    preview: preview in code for finding a waiting loop for planes

--- src/TrafficController.py
+++ src/TrafficController.py
@@ -7,8 +7,10 @@ class TrafficController:
         airspace Airspace : Airspace controlled by the Controller
         rule_set list[Rule] : list of rules the flight traffic controller has to comply with
         precalc_air_situation List[List[Plane Positions for time step]]: Calculation of flight-paths in the future
+        standard_routes list[Route] : stores Routes for planes to wait, approach, take off, etc.
     Functions:
         append_rule : args [new_rule:Rule] : appends new rule to rule_set list
+        create_waiting_loop : args [approach_route: Route, min_distance_to_runway: float] : tries to find a route in airspace where planes can safely wait on approach
     """
     def __init__(self, init_airspace:src.Airspace.Airspace, rules=None) -> None:
         self._airspace = init_airspace
@@ -19,6 +21,12 @@ class TrafficController:
 
     def append_rule(self, new_rule) -> None: #Add type hint, when rule class is implemented
         self._rule_set.append(new_rule)
+
+    def create_waiting_loop(self, approach_route, min_distance_to_runway) -> bool:
+        """
+        :return: success or not (bool)
+        """
+        pass
 
     @property
     def airspace(self) -> src.Airspace.Airspace:
