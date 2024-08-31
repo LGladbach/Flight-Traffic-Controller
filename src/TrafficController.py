81d05f58e8a8d38be077e67d60b896a3a9da3eda
Laurenz Gladbach <laurenz2@mail.uni-paderborn.de>
2024-08-31 11:45:24 +0200

    feat: added TrafficController class without core functionality, just some attribute management and migrated rule_set from airspace to Traffic Controller

---
+++ src/TrafficController.py
@@ -0,0 +1,37 @@
+import src.Airspace
+
+class TrafficController:
+    """
+    Implements the traffic control algorithm. (So far only for one runway, some very simple planes)
+    Attributes:
+        airspace Airspace : Airspace controlled by the Controller
+        rule_set list[Rule] : list of rules the flight traffic controller has to comply with
+        precalc_air_situation List[List[Plane Positions for time step]]: Calculation of flight-paths in the future
+    Functions:
+        append_rule : args [new_rule:Rule] : appends new rule to rule_set list
+    """
+    def __init__(self, init_airspace:src.Airspace.Airspace, rules=None):
+        self._airspace = init_airspace
+        if rules is None:
+            rules = []
+        self._rule_set = rules
+        self._precalc_air_situation = []
+
+    def append_rule(self, new_rule): #Add type hint, when rule class is implemented
+        self._rule_set.append(new_rule)
+
+    @property
+    def airspace(self):
+        return self._airspace
+
+    @airspace.setter
+    def airspace(self, new_airspace:src.Airspace.Airspace):
+        pass
+
+    @property
+    def rule_set(self):
+        return self._rule_set
+
+    @property
+    def precalc_air_situation(self):
+        return self._precalc_air_situation
